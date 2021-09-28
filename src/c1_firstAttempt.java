07013d947604439528b8ace475a8106286c2dd6e
Hamsa Elyas <safe-tec@Hamsas-MacBook-Air.local>
2021-09-28 01:09:54 -0700

    first commit

---
+++ src/c1_firstAttempt.java
@@ -0,0 +1,7 @@
+public class c1_firstAttempt {
+    public static void main(String[] args) {
+        System.out.println("This is my first attempt");
+        System.out.println("and hopfully it will be my last onegit");
+    }
+
+}
