e61ef1e50fd05a9434d072fbda203afcbb49ac91
Hamsa Elyas <safe-tec@Hamsas-MacBook-Air.local>
2021-09-28 10:14:51 -0700

    myfirst attempt

---
+++ src/c2_secondTry.java
@@ -0,0 +1,7 @@
+public class c2_secondTry {
+    public static void main(String[] args) {
+        System.out.println("This will the second class");
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+        System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%");
+    }
+}
