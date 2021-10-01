fa1c26c6f3345002082848f171e7818b372d8db8
HamsaElyas <91717099+HamsaElyas@users.noreply.github.com>
2021-10-01 01:51:54 -0700

    Update c5_newfile.java

--- src/c5_newfile.java
+++ src/c5_newfile.java
@@ -2,5 +2,6 @@ public class c5_newfile {
     public static void main(String[] args) {
         System.out.println("Hamsa");
         System.out.println("Elyas");
+        System.out.println("QA tester");
     }
 }
