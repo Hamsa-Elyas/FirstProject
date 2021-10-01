6d5e81211c48ea20b210a448788d17c32b2225e0
Hamsa Elyas <safe-tec@Hamsas-MacBook-Air.local>
2021-10-01 01:44:44 -0700

    adding a new println

--- src/c4_lastattempt.java
+++ src/c4_lastattempt.java
@@ -1,5 +1,6 @@
 public class c4_lastattempt {
     public static void main(String[] args) {
         System.out.println("last one");
+        System.out.println("another sentence to be added");
     }
 }
--- src/c5_newfile.java
+++ src/c5_newfile.java
@@ -1,5 +1,6 @@
 public class c5_newfile {
     public static void main(String[] args) {
         System.out.println("Hamsa");
+        System.out.println("Elyas");
     }
 }
